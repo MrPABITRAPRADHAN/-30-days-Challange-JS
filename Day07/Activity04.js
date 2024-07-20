94f7a12db135dc984e1758269cb1204fbf56a240
MrPABITRAPRADHAN <gpabitrapadhan@gmail.com>
2024-07-20 12:49:30 +0530

    Day 07 completed(object)

--- Day07/Activity04.js
+++ Day07/Activity04.js
@@ -2,11 +2,24 @@
 
 // add a metthod to the book object using 'this' kw return string with books title and year
 const book = {
-    title:"The power of subconscious mind ",
-    
-    year: 1963,
-    string: function(){
-         console.log("Book title- "+this.title +"Year of publish- "+this.year)
+   first:{
+
+        title:"The power of subconscious mind ",
+        
+        year: 1963,
+        string: function(){
+            console.log("Book title- "+this.title +"Year of publish- "+this.year)
+        },
     },
+    second:{
+        title:"subconscious mind ",
+        
+        year: 2020,
+        string: function(){
+            console.log("Book title- "+this.title +"Year of publish- "+this.year)
+        },
+    }
+
 }
-book.string();
+// book.string();
+book.second.string()
